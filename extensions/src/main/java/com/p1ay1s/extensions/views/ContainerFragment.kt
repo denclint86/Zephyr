f18498d4321ad0e727cee63c77413fc99959f616
Haowen <ponnamorevvloddimer@gmail.com>
2024-09-26 21:07:02 +0800

    fix(all):修复若干问题

--- extensions/src/main/java/com/p1ay1s/extensions/views/ContainerFragment.kt
+++ extensions/src/main/java/com/p1ay1s/extensions/views/ContainerFragment.kt
@@ -1,21 +1,39 @@
 package com.p1ay1s.extensions.views
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.p1ay1s.dev.ui.FragmentControllerView
-import com.p1ay1s.dev.viewbinding.ViewBindingFragment
 import com.p1ay1s.extensions.databinding.FragmentContainerBinding
 
 open class ContainerFragment(private val map: LinkedHashMap<String, Fragment>) :
-    ViewBindingFragment<FragmentContainerBinding>() {
+    Fragment() {
+
+    private lateinit var binding: FragmentContainerBinding
 
     var controllerView: FragmentControllerView? = null
 
-    override fun FragmentContainerBinding.initBinding() {
-        fragmentControllerViewContainer.run {
-            controllerView = this
-            fragmentManager = childFragmentManager
-            fragmentMap = map
-            init()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentContainerBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.run {
+            fragmentControllerViewContainer.run {
+                controllerView = this
+                fragmentManager = childFragmentManager
+                fragmentMap = map
+                init()
+            }
         }
     }
 }
