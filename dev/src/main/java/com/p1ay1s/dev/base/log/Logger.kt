cc33fae9cb285f600f420ec5a8927f12678a883a
p1ay1s <ponnamorevvloddimer@gmail.com>
2024-09-26 09:02:48 +0800

    更新 Logger.kt

--- dev/src/main/java/com/p1ay1s/dev/base/log/Logger.kt
+++ dev/src/main/java/com/p1ay1s/dev/base/log/Logger.kt
@@ -205,12 +205,18 @@ object Logger {
     }
 
     private fun registerHandler() {
+        /**
+         * 主线程捕获原理:
+         * 用 handler 给主线程的 looper 添加新的任务——进入一个不断 loop 的死循环,
+         * 此时原生的 loop 函数由于在内部不断执行我们的任务,
+         * 相当于被我们的 loop 接管了,
+         */ 而我们的 loop 被 try-catch 块包裹所以可以捕捉所有异常
         Handler(Looper.getMainLooper()).post {
-            while (!isCrashed) {
+            while (!isCrashed) { // 当捕捉并处理异常后不再阻塞原生的 loop, 以便启动崩溃详情 activity
                 try {
                     Looper.loop()
-                } catch (e: Throwable) {
-                    loggerCrashHandler(Looper.getMainLooper().thread, e)
+                } catch (t: Throwable) {
+                    loggerCrashHandler(Looper.getMainLooper().thread, t)
                 }
             }
         }
